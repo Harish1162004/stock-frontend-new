7ec9fa9fd7726be9bf0904938a55ac8def24bfd4
Harish1162004 <harish1162004@gmail.com>
2026-01-10 11:12:34 +0530

    update

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./signup.css"; // ✅ CSS added
+import "./signup.css";// ✅ CSS added
 
 function Signup() {
   const [username, setUsername] = useState("");
