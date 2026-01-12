3afe3415c06b347b33c29dd09d6995a7aafa2bee
Harish1162004 <harish1162004@gmail.com>
2026-01-12 19:34:49 +0530

    Fix missing routes and chart container height

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
+import Orders from "./orders";
+import PlaceOrder from "./PlaceOrder";
+import Portfolio from "./Portfolio";
 import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
+      {/* Protected */}
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Route>
     </Routes>
   );
